226bd142fd73d25fb78660a53e7ad95ce88bb82e
Himanshu Gupta <himanshu@knoldus.com>
2020-03-22 19:42:51 +0530

    Calling a Superclass Constructor

---
+++ classes-properties/src/main/scala/edu/knoldus/bootcamp/scala/SuperClassConstructorCalling.scala
@@ -0,0 +1,31 @@
+package edu.knoldus.bootcamp.scala
+
+class SuperClassConstructorCalling {
+  case class Address (city: String, state: String)
+
+  case class Role (role: String)
+
+  class Person (var name: String, var address: Address) {
+    // no way for Employee auxiliary constructors to call this constructor
+    def this (name: String) {
+      this(name, null)
+      address = null
+    }
+
+    override def toString = if (address == null) name else s"$name @ $address"
+  }
+
+  class Employee (name: String, role: Role, address: Address) extends Person (name, address) {
+    def this (name: String) {
+      this(name, null, null)
+    }
+
+    def this (name: String, role: Role) {
+      this(name, role, null)
+    }
+
+    def this (name: String, address: Address) {
+      this(name, null, address)
+    }
+  }
+}
